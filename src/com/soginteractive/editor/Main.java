6dc33f52c1469df973acb85708255c5a11ee5de4
bdulaney88 <draygera@gmail.com>
2014-07-04 00:34:54 -0500

    Issue #1 Init

--- src/com/soginteractive/editor/Main.java
+++ src/com/soginteractive/editor/Main.java
@@ -1,17 +1,56 @@
 package com.soginteractive.editor;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglAWTCanvas;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.soginteractive.game.archAdv.core.GameMain;
 
-public class Main {
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final Container container = getContentPane();
+		container.setLayout(new BorderLayout());
+
+		LwjglAWTCanvas canvas = new LwjglAWTCanvas(new EditorMain());
+		container.add(canvas.getCanvas(), BorderLayout.CENTER);
+
+		JPanel panel = new JPanel(new GridLayout(2, 4));
+
+		JButton button = new JButton("Sex is good");
+		JButton button2 = new JButton("But Food is Better.");
+		//JButton button3 = new JButton("Aye, Aye, Aye.");
+		button.setSize(25, 50);
+		// button.setVisible(true);
+
+		panel.add(button);
+		panel.add(button2);
+		//panel.add(button3);
+		container.add(panel, BorderLayout.WEST);
+
+		pack();
+		setVisible(true);
+		setSize(800, 600);
+
+	}
 
 	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "ArchengeiaAdventuresEditor";
-		cfg.width = 800;
-		cfg.height = 600;
-		cfg.resizable = true;
-		
-		new LwjglApplication(new EditorMain(), cfg);
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Main();
+			}
+		});
 	}
 }
