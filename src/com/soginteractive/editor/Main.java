49e428933d5d4ac71b6160fce231ece891ed9762
bdulaney88 <draygera@gmail.com>
2014-08-04 16:58:24 -0500

    Issue #5 Init
    Started making some basic menus.

--- src/com/soginteractive/editor/Main.java
+++ src/com/soginteractive/editor/Main.java
@@ -26,16 +26,52 @@ public class Main extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 
 		JMenu menu = new JMenu("File");
-		JMenuItem item = new JMenuItem("New Project");
+		JMenu menu2 = new JMenu("Edit");
+		JMenu menu3 = new JMenu("Window");
 
-		JMenu menu2 = new JMenu("Window");
-		JMenuItem item2 = new JMenuItem("Main");
+		JMenuItem menuItem1 = new JMenuItem("New Project");
+		JMenuItem menuItem2 = new JMenuItem("Open Project");
+		JMenuItem menuItem3 = new JMenuItem("Save Project");
+		JMenuItem menuItem4 = new JMenuItem("Close Project");
 
-		menu.add(item);
-		menu2.add(item2);
+		JMenuItem menu2Item1 = new JMenuItem("Cut");
+		JMenuItem menu2Item2 = new JMenuItem("Copy");
+		JMenuItem menu2Item3 = new JMenuItem("Paste");
+
+		JMenuItem menu3Item1 = new JMenuItem("Main");
+
+		JMenu menu3Submenu1 = new JMenu("Editors");
+
+		JMenuItem m3S1Item1 = new JMenuItem("Map");
+		JMenuItem m3S1Item2 = new JMenuItem("Character");
+		JMenuItem m3S1Item3 = new JMenuItem("Enemy");
+		JMenuItem m3S1Item4 = new JMenuItem("Equipment");
+		JMenuItem m3S1Item5 = new JMenuItem("Item");
+		JMenuItem m3S1Item6 = new JMenuItem("Event");
+
+		menu.add(menuItem1);
+		menu.add(menuItem2);
+		menu.add(menuItem3);
+		menu.add(menuItem4);
+
+		menu2.add(menu2Item1);
+		menu2.add(menu2Item2);
+		menu2.add(menu2Item3);
+
+		menu3.add(menu3Item1);
+
+		menu3Submenu1.add(m3S1Item1);
+		menu3Submenu1.add(m3S1Item2);
+		menu3Submenu1.add(m3S1Item3);
+		menu3Submenu1.add(m3S1Item4);
+		menu3Submenu1.add(m3S1Item5);
+		menu3Submenu1.add(m3S1Item6);
+
+		menu3.add(menu3Submenu1);
 
 		menuBar.add(menu);
 		menuBar.add(menu2);
+		menuBar.add(menu3);
 
 		container.add(menuBar, BorderLayout.NORTH);
 
