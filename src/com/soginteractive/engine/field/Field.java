91ca1772954be775704572b87ee87d0c3ba34582
draygera <draygera@gmail.com>
2014-10-05 15:52:12 -0500

    Field Scripting Work

--- src/com/soginteractive/engine/field/Field.java
+++ src/com/soginteractive/engine/field/Field.java
@@ -1,8 +1,17 @@
 package com.soginteractive.engine.field;
 
-public class Field {
+import static com.soginteractive.engine.core.util.ScriptUtils.printString;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.Json.Serializable;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class Field implements Serializable {
 
 	private String name;
+
+	private final String OBJ = "field";
+	private final String NAME = "name";
 
 	public Field() {
 		this("");
@@ -19,6 +28,25 @@ public class Field {
 
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public void write(Json json) {
+		json.writeObjectStart(OBJ);
+		{
+			json.writeValue(NAME, name);
+		}
+		json.writeObjectEnd();
+	}
+
+	@Override
+	public void read(Json json, JsonValue value) {
+		JsonValue child = value.child;
+		name(child.getString(NAME));
+	}
+
+	public void printJson() {
+		printString(NAME, name);
 	}
 
 }
