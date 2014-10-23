764703e5867dbb160151688479e6e6fbb8b3914d
Brandon <Draygera@gmail.com>
2014-10-23 02:16:13 -0500

    SpreadSheet Reader Work

---
+++ src/com/soginteractive/editor/util/SpreadsheetReader.java
@@ -0,0 +1,15 @@
+package com.soginteractive.editor.util;
+
+import com.soginteractive.engine.core.AbstractScripter;
+
+public class SpreadsheetReader extends AbstractScripter {
+
+	public SpreadsheetReader(String path) {
+		super(path);
+	}
+
+	public void readSpreadSheet() {
+		
+	}
+
+}
