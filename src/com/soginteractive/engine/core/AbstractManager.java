159f9e7a510513e5055130c3a419bed5cf95d2b0
Brandon <Draygera@gmail.com>
2014-11-05 16:11:06 -0600

    Scripting Cleanup Init
    Started working on implementing methods into the Manager classes in
    order to make the scripting more efficient and hopefully fix some bugs.
    (Can't believe that I didn't think of this earlier...)

--- src/com/soginteractive/engine/core/AbstractManager.java
+++ src/com/soginteractive/engine/core/AbstractManager.java
@@ -1,11 +1,18 @@
 package com.soginteractive.engine.core;
 
+import com.badlogic.gdx.utils.Array;
+
 public abstract class AbstractManager implements Manager {
 
 	protected String path;
 
+	protected Array<Scripter> writers;
+	protected Array<Scripter> readers;
+
 	public AbstractManager(String path) {
 		path(path);
+		writers = new Array<Scripter>();
+		readers = new Array<Scripter>();
 	}
 
 	@Override
@@ -16,14 +23,66 @@ public abstract class AbstractManager implements Manager {
 	}
 
 	private void checkPath() {
-		if (!path.endsWith("/")) {
+		if (!path.endsWith("/") || !path.endsWith("\\")) {
 			path = path.concat("/");
 		}
 	}
 
 	@Override
+	public Manager writer(Scripter writer) {
+		writers.add(writer);
+		return this;
+	}
+
+	@Override
+	public Manager reader(Scripter reader) {
+		readers.add(reader);
+		return this;
+	}
+
+	@Override
+	public void writeAllScripts() {
+
+	}
+
+	@Override
+	public void writeScript(Scripter writer) {
+
+	}
+
+	@Override
+	public void writeScript(Scripter writer, Object object) {
+
+	}
+
+	@Override
+	public void readAllScripts() {
+
+	}
+
+	@Override
+	public void readScript(Scripter reader) {
+
+	}
+
+	@Override
+	public void readScript(Scripter reader, Object object) {
+
+	}
+
+	@Override
 	public String getPath() {
 		return path;
+	}
+
+	@Override
+	public Array<Scripter> getWriters() {
+		return writers;
+	}
+
+	@Override
+	public Array<Scripter> getReaders() {
+		return readers;
 	}
 
 	protected void checkPath(Manager manager) {
--- src/com/soginteractive/engine/core/Manager.java
+++ src/com/soginteractive/engine/core/Manager.java
@@ -1,9 +1,31 @@
 package com.soginteractive.engine.core;
 
+import com.badlogic.gdx.utils.Array;
+
 public interface Manager {
 
 	Manager path(String path);
+
+	abstract Manager writer(Scripter writer);
+
+	abstract Manager reader(Scripter reader);
+
+	abstract void writeAllScripts();
+
+	abstract void writeScript(Scripter writer);
 	
+	abstract void writeScript(Scripter writer, Object object);
+
+	abstract void readAllScripts();
+
+	abstract void readScript(Scripter reader);
+	
+	abstract void readScript(Scripter reader, Object object);
+
 	String getPath();
+
+	abstract Array<Scripter> getWriters();
+
+	abstract Array<Scripter> getReaders();
 
 }
